1baa15d7bf4de5b70bb3e759341d21ae3355313e
Wout Vinckevleugel <wout.vinck@gmail.com>
2023-02-18 16:19:23 +0100

    Fully functioning tracker

--- app/vault/initiativeTracker.tsx
+++ app/vault/initiativeTracker.tsx
@@ -26,7 +26,7 @@ class InitiativeList extends React.Component<{}, any> {
 
     addItem() {
         const inputList = this.state.inputList;
-        let className:string = "initiative-item"
+        let className: string = "initiative-item"
         const itemToAdd = (
             <div className={className} key={inputList.length} id={inputList.length}>
                 <div className="md:flex md:items-center mb-6">
@@ -46,28 +46,28 @@ class InitiativeList extends React.Component<{}, any> {
                     </div>
                 </div>
             </div>
-            )
+        )
 
         this.setState({
             inputList: inputList.concat(itemToAdd)
         });
 
+        this.activeCharacter = 0;
+        this.nextCharacter = 1;
         for (let index = 0; index < inputList.length; index++) {
-            if(index != 0) document.getElementById(index.toString())?.classList.remove("active")
-            else document.getElementById(index.toString())?.classList.add("active")
+            document.getElementById(index.toString())?.classList.remove("active")
         }
+        document.getElementById("0")?.classList.add("active")
     }
 
-    next(){
+    next() {
         const inputList = this.state.inputList;
         document.getElementById(this.activeCharacter.toString())?.classList.toggle("active")
         document.getElementById(this.nextCharacter.toString())?.classList.toggle("active")
-        if(this.nextCharacter == inputList.length - 1){this.nextCharacter = 0}
-        else{this.nextCharacter++}
-        if(this.activeCharacter == inputList.length - 1){this.activeCharacter = 0}
-        else{this.activeCharacter++}
-        // inputList[this.nextCharacter]
-        // inputList[this.nextCharacter].classList
+        if (this.nextCharacter == inputList.length - 1) { this.nextCharacter = 0 }
+        else { this.nextCharacter++ }
+        if (this.activeCharacter == inputList.length - 1) { this.activeCharacter = 0 }
+        else { this.activeCharacter++ }
     }
     render(): React.ReactNode {
         return (
@@ -75,7 +75,7 @@ class InitiativeList extends React.Component<{}, any> {
                 <button type="button" onClick={this.addItem}>Add a character</button>
                 <div></div>
                 <button type="button" onClick={this.next}>Next</button>
-                
+
                 {this.state.inputList.map(function (input, index) {
 
                     return input;
@@ -84,39 +84,3 @@ class InitiativeList extends React.Component<{}, any> {
         );
     }
 }
-
-// class InitiativeItem extends React.Component {
-//     className:string = "initiative-item";
-//     constructor(props: {}){
-//         super(props)
-//     }
-    
-
-//     isNext() {
-//         this.className = "initiative-item active"
-//     }
-    
-//     render() {
-//         this.isNext()
-//         return (
-//             <div className={this.className}>
-//                 <div className="md:flex md:items-center mb-6">
-//                     <div className="md:w-1/3">
-//                         <label htmlFor="name" className="block text-gray-500 font-bold labelText mb-1 md:mb-0 pr-4">Name</label>
-//                     </div>
-//                     <div className="md:w-2/3 items-end">
-//                         <input id="name" type={"text"} className="inputText bg-gray-100 appearance-none w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" />
-//                     </div>
-//                 </div>
-//                 <div className="md:flex md:items-center mb-6">
-//                     <div className="md:w-1/3">
-//                         <label htmlFor="health" className="block text-gray-500 font-bold labelText mb-1 md:mb-0 pr-4">HP</label>
-//                     </div>
-//                     <div className="md:w-2/3 items-end">
-//                         <input id="health" type={"number"} min="0" className="inputText bg-gray-100 appearance-none w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" />
-//                     </div>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
