b25feaabcacc1e4a1d41638c52ee0881acaa51f8
snenenenenenene <sennebels@gmail.com>
2023-02-21 12:16:29 +0100

    perf: :card_file_box: completely refactored store to be more readable and expandable

--- app/utils/store.ts
+++ app/utils/store.ts
@@ -1,68 +1,138 @@
 /* eslint-disable no-unused-vars */
+// import { classes } from "@/data/classes/classes";
+// import { Class } from "@/data/classes/types";
+// import { races } from "@/data/races/races";
+// import { Race } from "@/data/races/types";
 import { classes } from "@/data/classes/classes";
 import { Class } from "@/data/classes/types";
 import { races } from "@/data/races/races";
 import { Race } from "@/data/races/types";
 import { create } from "zustand";
-import { devtools, persist } from "zustand/middleware";
-import { Sheet } from "../components/sheets/List";
+import { persist, devtools } from "zustand/middleware";
+// import { Sheet } from "../components/sheets/List";
 
-interface sheetstate {
-  sheets: Array<Sheet>;
-  currentSheet: Sheet;
-  setRace: (race: Race) => void;
-  setClass: (_class: Class[] | []) => void;
-  removeClass: (index: number) => void;
-  setCurrentSheet: (name: string) => void;
-  updateCurrentSheet: (key: string, value: any) => void;
+// interface State {
+//   sheets: Array<Sheet>;
+//   selectedSheet: Sheet | null;
+//   setSelectedSheet: (sheet: Sheet | null) => void;
+//   selectSheetByName: (name: string) => any;
+//   updateSelectedSheet: (update: SheetUpdate) => void;
 
-  addSheet: (sheet: Sheet) => void;
+//   addSheet: (sheet: Sheet) => void;
+// }
+
+export interface Sheet {
+  name: string;
+  race: Race;
+  class: Class[];
+}
+interface SheetUpdate {
+  name?: string;
+  race?: Race;
+  class?: Class[];
 }
 
-export const useSheetStore = create<sheetstate>()(
+// export interface Store {
+//   sheets: Sheet[];
+//   selectedSheet: Sheet | null;
+//   selectSheet: (name: string) => void;
+//   updateSelectedSheet: (update: SheetUpdate) => void;
+//   addSheet: (name: string) => void;
+// }
+
+type StoreState = {
+  sheets?: Sheet[] | any;
+  selectedSheet?: Sheet | null | any;
+  selectSheet?: (name: string) => void | any;
+  updateSelectedSheet?: (update: SheetUpdate) => void | any;
+  addSheet?: (name: string) => void | any;
+};
+
+export const useSheetStore = create<StoreState>()(
   devtools(
     persist(
       (set) => ({
-        sheets: [
-          { name: "Aperepne", race: races[1], class: [classes[0], classes[1]] },
-        ],
-        setCurrentSheet: (name: string) =>
+        sheets: [],
+        selectedSheet: null,
+        selectSheet: (name: string) =>
           set((state) => ({
-            currentSheet: state.sheets.find((s) => name === s.name),
+            selectedSheet:
+              state.sheets.find((sheet: Sheet) => sheet.name === name) || null,
           })),
-        updateCurrentSheet: (key, value) =>
-          set((state) => ({
-            currentSheet: { ...state.currentSheet, key: value },
-          })),
-        currentSheet: {
-          name: "default",
-          race: races[0],
-          class: [classes[0], classes[1]],
-        },
-        setRace: (race: Race) =>
-          set((state: any) => ({
-            currentSheet: { ...state.currentSheet, race: race },
-          })),
-        setClass: (_class: Class[]) =>
-          set((state: any) => ({
-            currentSheet: { ...state.currentSheet, class: _class },
-          })),
-        removeClass: (index: number) =>
-          set((state: any) => ({
-            currentSheet: {
-              ...state.currentSheet,
-              class: state.currentSheet.class.filter(
-                (c: Class, i: number) => i !== index
-              ),
-            },
-          })),
-        addSheet: (sheet) =>
-          set((state) => ({ sheets: [...state.sheets, sheet] })),
+        updateSelectedSheet: (update: SheetUpdate) =>
+          set((state) => {
+            if (state.selectedSheet) {
+              const updatedSheet = {
+                ...state.selectedSheet,
+                ...update,
+              };
+              const sheets = state.sheets.map((sheet: Sheet) =>
+                sheet.name === updatedSheet.name ? updatedSheet : sheet
+              );
+              return {
+                ...state,
+                sheets,
+                selectedSheet: updatedSheet,
+              };
+            }
+            return state;
+          }),
+        addSheet: (name: string) =>
+          set((state) => {
+            const sheet: Sheet = {
+              name,
+              race: races[0],
+              class: [classes[0], classes[1]],
+            };
+            return {
+              sheets: [...state.sheets, sheet],
+              selectedSheet: sheet,
+            };
+          }),
       }),
-
       {
         name: "sheet-storage",
       }
     )
   )
 );
+
+// export const useSheetStore = create<StoreState>((set, _get, api) => ({
+//   //TODO: add this to the old store
+//   sheets: [],
+//   selectedSheet: null,
+//   selectSheet: (name: string) =>
+//     set((state) => ({
+//       selectedSheet: state.sheets.find((sheet) => sheet.name === name) || null,
+//     })),
+//   updateSelectedSheet: (update: SheetUpdate) =>
+//     set((state) => {
+//       if (state.selectedSheet) {
+//         const updatedSheet = {
+//           ...state.selectedSheet,
+//           ...update,
+//         };
+//         const sheets = state.sheets.map((sheet) =>
+//           sheet.name === updatedSheet.name ? updatedSheet : sheet
+//         );
+//         return {
+//           ...state,
+//           sheets,
+//           selectedSheet: updatedSheet,
+//         };
+//       }
+//       return state;
+//     }),
+//   addSheet: (name: string) =>
+//     set((state) => {
+//       const sheet: Sheet = {
+//         name,
+//         race: races[0],
+//         class: [classes[0], classes[1]],
+//       };
+//       return {
+//         sheets: [...state.sheets, sheet],
+//         selectedSheet: sheet,
+//       };
+//     }),
+// }));
