a8af198b72ea47c9bbff8cbb033b2f90fee22221
snenenenenenene <sennebels@gmail.com>
2023-02-27 23:46:53 +0100

    style: :lipstick: adding icon + login/signup banners

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Dungeon Crafter</title>
       <meta name="description" content="Character creation tool" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/assets/d20.png" />
     </head>
   );
 }
