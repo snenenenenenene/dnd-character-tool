47a87823c3b1a8bb59c7d25be411aa024ba567ec
snenenenenenene <sennebels@gmail.com>
2023-03-02 09:39:22 +0100

    feat: :truck: renamed slug to [id] + added dark theme + added pocketbase compatibility

---
+++ app/sheets/[id]/page.tsx
@@ -0,0 +1,180 @@
+"use client";
+
+import { getSheetWithId, updateSheetWithId } from "@/app/utils/apiCalls";
+import { Sheet } from "@/app/utils/store";
+import { races } from "@/data/races/races";
+import { Race } from "@/data/races/types";
+import _ from "lodash";
+import Image from "next/image";
+
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { GiBoomerang, GiBroadheadArrow } from "react-icons/gi";
+import { useStateWithCallbackLazy } from "use-state-with-callback";
+
+export default function Sheets(context: any) {
+  const [sheet, setSheet] = useState<{
+    data: Sheet;
+    campaign: string;
+    user: string;
+  } | null>(null);
+
+  useEffect(() => {
+    getSheetWithId(context.params.id)
+      .then((res: any) => {
+        setSheet(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const groups = _.groupBy(races, "expansion");
+
+  return (
+    <div className="w-full relative h-full flex flex-col">
+      <section
+        id="title"
+        className="w-full py-5 flex items-center border-b-2 pl-10 border-light-secondary"
+      >
+        <h1 className="text-xl font-semibold ">Pick your race</h1>
+      </section>
+
+      <section className="flex flex-col px-10">
+        {Object.entries(groups).map(([expansion, races]) => {
+          return (
+            <>
+              <h3 className="font-bold uppercase py-4" key={expansion}>
+                {expansion}
+              </h3>
+              <section className="grid gap-4 grid-cols-3 mx-auto">
+                {races.map((race: Race) => (
+                  <button
+                    className={`${
+                      sheet?.data?.race?.name === race.name
+                        ? "border-light-accent shadow-lg"
+                        : "border-light-primary"
+                    }  border-2  transition-all filter hover:drop-shadow-lg flex justify-center items-center hover:border-light-secondary h-48 w-60 flex-col py-1`}
+                    key={race?.name}
+                    value={race?.name}
+                    onClick={() => {
+                      updateSheetWithId(
+                        context.params.id,
+                        {
+                          ...sheet?.data,
+                          race: race,
+                        },
+                        sheet?.campaign,
+                        sheet?.user!
+                      )
+                        .then((res: any) => {
+                          console.log(res);
+                          setSheet(res);
+                        })
+                        .catch((err) => {
+                          console.log(err);
+                        });
+
+                      // setSheet((prev: any) => ({
+
+                      //   ...prev,
+                      //   data: {
+                      //     ...prev?.data,
+                      //     race: race,
+                      //     stats: {
+                      //       strength:
+                      //         Math.floor(
+                      //           (Number(prev?.data?.thrownStats?.strength) +
+                      //             10) /
+                      //             2
+                      //         ) +
+                      //         Number(
+                      //           prev?.data?.race?.traits?.abilityScoreIncrease
+                      //             ?.strength || 0
+                      //         ),
+                      //       dexterity:
+                      //         Math.floor(
+                      //           (Number(prev?.data?.thrownStats?.dexterity) +
+                      //             10) /
+                      //             2
+                      //         ) +
+                      //         Number(
+                      //           prev?.data?.race?.traits?.abilityScoreIncrease
+                      //             ?.dexterity || 0
+                      //         ),
+                      //       constitution:
+                      //         Math.floor(
+                      //           (Number(prev?.data?.thrownStats?.constitution) +
+                      //             10) /
+                      //             2
+                      //         ) +
+                      //         Number(
+                      //           prev?.data?.race?.traits?.abilityScoreIncrease
+                      //             ?.constitution || 0
+                      //         ),
+                      //       intelligence:
+                      //         Math.floor(
+                      //           (Number(prev?.data?.thrownStats?.intelligence) +
+                      //             10) /
+                      //             2
+                      //         ) +
+                      //         Number(
+                      //           prev?.data?.race?.traits?.abilityScoreIncrease
+                      //             ?.intelligence || 0
+                      //         ),
+                      //       wisdom:
+                      //         Math.floor(
+                      //           (Number(prev?.data?.thrownStats?.wisdom) + 10) /
+                      //             2
+                      //         ) +
+                      //         Number(
+                      //           prev?.data?.race?.traits?.abilityScoreIncrease
+                      //             ?.wisdom || 0
+                      //         ),
+                      //       charisma:
+                      //         Math.floor(
+                      //           (Number(prev?.data?.thrownStats?.charisma) +
+                      //             10) /
+                      //             2
+                      //         ) +
+                      //         Number(
+                      //           prev?.data?.race?.traits?.abilityScoreIncrease
+                      //             ?.charisma || 0
+                      //         ),
+                      //     },
+                      //   },
+                      // })),
+                      //   () => updateSheet();
+                    }}
+                  >
+                    {/* // TODO: ADD NEXTJS IMAGE TAG */}
+                    <Image
+                      className="w-36 h-36 object-contain"
+                      width={144}
+                      height={144}
+                      alt={race.name}
+                      src={race?.picture!}
+                    />
+                    <h4 className="uppercase font-medium mt-2">{race.name}</h4>
+                  </button>
+                ))}
+              </section>
+            </>
+          );
+        })}
+      </section>
+      <Link
+        href={`/sheets/${context.params.id}/class`}
+        className="fixed bottom-32 z-50 right-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-light-text w-12 h-12 rounded-full flex justify-center items-center hover:to-cyan-500 transition-colors text-xl"
+      >
+        <GiBroadheadArrow />
+      </Link>
+      <Link
+        href={"/sheets"}
+        className="fixed bottom-32 left-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-light-text w-12 h-12 rounded-full flex justify-center items-center hover:to-cyan-500 transition-colors text-xl"
+      >
+        <GiBoomerang />
+      </Link>
+    </div>
+  );
+}
--- app/vault/[id]/page.tsx
+++ app/vault/[id]/page.tsx
@@ -5,6 +5,15 @@ import PocketBase from "pocketbase";
 import Select from "react-select";
 import { useSheetStore } from "@/app/utils/store";
 import { toast } from "react-toastify";
+import InitiativeTracker from "../../components/vault/initiativeTracker";
+import {
+  getAllUsers,
+  getCampaignById,
+  getSheetWithCampaignIdAndUserId,
+  getUserFromUserId,
+  updateCampaign,
+} from "@/app/utils/apiCalls";
+import { Race } from "@/data/races/types";
 
 function UserEntry({
   userId,
@@ -13,32 +22,26 @@ function UserEntry({
   userId: string;
   campaignId: string;
 }) {
-  const pb = new PocketBase(process.env.NEXT_PUBLIC_API_URL);
-  const [user, setUser] = useState({});
-  const [sheet, setSheet] = useState({});
-  useEffect(() => {
-    pb.collection("users")
-      .getOne(userId)
-      .then((res: any) => {
-        setUser(res);
-      });
-  }, []);
+  const [user, setUser] = useState<{ username: string }>();
+  const [sheet, setSheet] = useState<{ name: string; race: Race }>();
 
   useEffect(() => {
-    //get all sheets where the campaign.id is equal to the campaignId
-    console.log(userId);
-    pb.collection("sheets")
-      .getFirstListItem(`campaign.id="${campaignId}" && user.id="${userId}"`)
-      .then((resp) => {
-        console.log(resp);
-        setSheet(resp.data);
-      });
+    getUserFromUserId(userId).then((res: any) => {
+      setUser(res);
+    });
+    getSheetWithCampaignIdAndUserId(campaignId, userId).then((resp) => {
+      console.log(resp);
+      setSheet(resp.data);
+    });
   }, []);
 
   return (
-    <div className="w-full flex justify-between items-center">
-      <p>{user.username}</p>
-      {sheet && <p>{sheet.name}</p>}
+    <div className="w-full pr-10 flex items-center gap-8">
+      <img className="object-contain w-32 h-32" src={sheet?.race?.picture} />
+      {sheet && (
+        <p className="uppercase font-semibold text-3xl">{sheet?.name}</p>
+      )}
+      <p className="ml-auto">{user?.username}</p>
     </div>
   );
 }
@@ -46,109 +49,112 @@ function UserEntry({
 export default function Campaign(context: any) {
   const pb = new PocketBase(process.env.NEXT_PUBLIC_API_URL);
 
+  const updateUsers = useSheetStore((state) => state.updateUsers);
   const [openModal, setOpenModal] = useState<boolean>(false);
   const [options, setOptions] = useState([]);
-  const [campaign, setCampaign] = useState({});
+  const [campaign, setCampaign] = useState<{ name: string; users: string[] }>();
   const [users, setUsers] = useState([]);
-
   const user = useSheetStore((state) => state.user);
 
   function toggleModal() {
     setOpenModal((openModal) => !openModal);
-    pb.collection("users")
-      .getFullList(200)
-      .then((res: any) => {
-        const arr: any = [];
-        res.map((user: any) => {
-          return arr.push({ value: user.id, label: user.username });
-        });
-        setOptions(arr);
+    getAllUsers().then((res) => {
+      const arr: any = [];
+      res.map((user: any) => {
+        return arr.push({ value: user.id, label: user.username });
       });
+      setOptions(arr);
+    });
   }
 
-  function updateUsers() {
+  function DoUpdateUsers() {
+    updateUsers(users, context.params.id, campaign);
     const data = {
       users: users,
       dm: user.record.id,
-      name: campaign.name,
+      name: campaign?.name,
     };
 
-    pb.collection("campaigns")
-      .update(context.params.id, data)
-      .then((resp) => {
-        console.log(resp);
+    updateCampaign(context.params.id, data)
+      .then((resp: any) => {
         setCampaign(resp);
         toast.success("Campaign updated!");
       })
       .catch((err: any) => {
-        console.log(err);
+        console.error(err);
       });
     toggleModal();
   }
 
   useEffect(() => {
-    pb.collection("campaigns")
-      .getOne(context.params.id)
+    getCampaignById(context.params.id)
       .then((res: any) => {
         console.log(res);
         setCampaign(res);
+      })
+      .catch((err: any) => {
+        console.error(err);
       });
   }, []);
 
   return (
-    <div className="w-full h-full">
-      <button
-        className="w-full flex justify-center items-center h-10 border-b-2 border-light-primary hover:bg-light-primary px-8 py-3 hover:text-light-text"
-        onClick={() => toggleModal()}
-      >
-        Add user
-      </button>
-      {campaign.users && (
-        <div className="w-full flex flex-col gap-4">
-          {campaign.users.map((userId: string) => (
-            <UserEntry
-              campaignId={context.params.id}
-              userId={userId}
-              key={userId}
-            />
-          ))}
-        </div>
-      )}
-
-      {openModal && (
-        <div
-          className="w-screen flex justify-center items-center h-screen absolute inset-0 bg-[#00000060]"
-          typeof="button"
-          data-value="parent"
-          onClick={(event: any) => {
-            event.preventDefault();
-            let dataValue = (event.target as HTMLElement).getAttribute(
-              "data-value"
-            );
-            if (dataValue === "parent") {
-              setOpenModal((openModal) => !openModal);
-            }
-          }}
+    <div className="w-full h-full flex">
+      <div className="w-full h-full">
+        <button
+          className="w-full flex justify-center items-center h-10 border-b-2 border-light-primary hover:bg-light-primary px-8 py-3 hover:text-light-text"
+          onClick={() => toggleModal()}
         >
-          <div
-            data-value="child"
-            className="w-1/3 h-1/2 min-w-[500px] flex flex-col justify-center gap-8 items-center bg-light-secondary rounded-md"
-          >
-            <Select
-              isMulti={true}
-              options={options}
-              onChange={(users: any) =>
-                setUsers(users.map((user: any) => user.value))
-              }
-              className="basic-multi-select"
-              classNamePrefix="select"
-              noOptionsMessage={() => "No users"}
-            />
-
-            <Button onClick={() => updateUsers()}>Confirm</Button>
+          Add user
+        </button>
+        {campaign?.users && (
+          <div className="w-full flex flex-col gap-4">
+            {campaign?.users?.map((userId: string) => (
+              <UserEntry
+                campaignId={context.params.id}
+                userId={userId}
+                key={userId}
+              />
+            ))}
           </div>
-        </div>
-      )}
+        )}
+
+        {openModal && (
+          <div
+            className="w-screen flex justify-center items-center h-screen absolute inset-0 bg-[#00000060]"
+            typeof="button"
+            data-value="parent"
+            onClick={(event: any) => {
+              event.preventDefault();
+              let dataValue = (event.target as HTMLElement).getAttribute(
+                "data-value"
+              );
+              if (dataValue === "parent") {
+                setOpenModal((openModal) => !openModal);
+              }
+            }}
+          >
+            <div
+              data-value="child"
+              className="w-1/3 h-1/2 min-w-[500px] flex flex-col justify-center gap-8 items-center bg-light-secondary rounded-md"
+            >
+              <Select
+                isMulti={true}
+                options={options}
+                onChange={(users: any) =>
+                  setUsers(users.map((user: any) => user.value))
+                }
+                className="basic-multi-select w-3/4"
+                classNamePrefix="select"
+                placeholder="Select users"
+                noOptionsMessage={() => "No users"}
+              />
+
+              <Button onClick={() => DoUpdateUsers()}>Confirm</Button>
+            </div>
+          </div>
+        )}
+      </div>
+      <InitiativeTracker />
     </div>
   );
 }
