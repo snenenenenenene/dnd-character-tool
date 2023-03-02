fb57950dd7356f5805bc99ad0efc062b2e27fb84
snenenenenenene <sennebels@gmail.com>
2023-03-02 13:25:00 +0100

    fix: :alien: made all api calls async

--- app/utils/apiCalls.ts
+++ app/utils/apiCalls.ts
@@ -3,53 +3,53 @@ import PocketBase from "pocketbase";
 const pb = new PocketBase(process.env.NEXT_PUBLIC_API_URL);
 pb.autoCancellation(false);
 
-export function getAllSheets({ userId }: { userId: string }) {
+export async function getAllSheets({ userId }: { userId: string }) {
   console.log(userId);
-  return pb
+  return await pb
     .collection("sheets")
     .getFullList(200, { filter: `user.id = "${userId}"` });
 }
 
-export function getAllUsers() {
-  return pb.collection("users").getFullList(200);
+export async function getAllUsers() {
+  return await pb.collection("users").getFullList(200);
 }
 
-export function getAllCampaigns() {
-  return pb.collection("campaigns").getFullList(200);
+export async function getAllCampaigns() {
+  return await pb.collection("campaigns").getFullList(200);
 }
 
-export function getUserFromUserId(userId: string) {
-  return pb.collection("users").getOne(userId);
+export async function getUserFromUserId(userId: string) {
+  return await pb.collection("users").getOne(userId);
 }
 
-export function getSheetWithCampaignIdAndUserId(
+export async function getSheetWithCampaignIdAndUserId(
   campaignId: string,
   userId: string
 ) {
-  return pb
+  return await pb
     .collection("sheets")
     .getFirstListItem(`campaign.id="${campaignId}" && user.id="${userId}"`);
 }
 
-export function updateCampaign(campaignId: string, data: any) {
-  return pb.collection("campaigns").update(campaignId, data);
+export async function updateCampaign(campaignId: string, data: any) {
+  return await pb.collection("campaigns").update(campaignId, data);
 }
 
-export function getCampaignById(campaignId: string) {
-  return pb.collection("campaigns").getOne(campaignId);
+export async function getCampaignById(campaignId: string) {
+  return await pb.collection("campaigns").getOne(campaignId);
 }
 
-export function getSheetWithId(sheetId: string) {
-  return pb.collection("sheets").getOne(sheetId);
+export async function getSheetWithId(sheetId: string) {
+  return await pb.collection("sheets").getOne(sheetId);
 }
 
-export function updateSheetWithId(
+export async function updateSheetWithId(
   sheetId: string,
   sheet: any,
   campaignId: string | undefined,
   userId: string
 ) {
-  return pb.collection("sheets").update(sheetId, {
+  return await pb.collection("sheets").update(sheetId, {
     data: sheet,
     campaign: campaignId ? campaignId : null,
     user: userId,
