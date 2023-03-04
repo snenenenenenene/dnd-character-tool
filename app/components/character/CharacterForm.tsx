1c4388cc4db431fe30f0dfbb634bf5213a0e8bbf
snenenenenenene <sennebels@gmail.com>
2023-03-04 10:06:57 +0100

    feat: :card_file_box: added full pocketbase support

--- app/components/character/CharacterForm.tsx
+++ app/components/character/CharacterForm.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
-import { useSheetStore } from "@/app/utils/store";
+import { updateSheetWithId } from "@/app/utils/apiCalls";
+import { Sheet, useSheetStore } from "@/app/utils/store";
 import { Class } from "@/data/classes/types";
 import { Race } from "@/data/races/types";
 import React, { useState } from "react";
@@ -9,25 +10,48 @@ import { Input } from "../common/Input";
 interface CharacterFormArgs {
   currClass: any;
   classIndex: number;
+  setSheet: React.Dispatch<any>;
+  sheet: {
+    data: Sheet;
+    campaign: string;
+    user: string;
+  };
+  sheetId: string;
 }
 
-export const CharacterForm = ({ currClass, classIndex }: CharacterFormArgs) => {
+export const CharacterForm = ({
+  currClass,
+  classIndex,
+  sheet,
+  setSheet,
+  sheetId,
+}: CharacterFormArgs) => {
   const [level, setLevel] = useState<number>(0);
+  const [featuresVisible, setFeaturesVisible] = useState<boolean>(false);
 
-  const selectedSheet = useSheetStore((state) => state.selectedSheet);
-  const updateSelectedSheet = useSheetStore(
-    (state) => state.updateSelectedSheet
-  );
   return (
-    <div className="flex flex-col w-1/2 relative gap-4" key={currClass?.name}>
+    <div className="flex flex-col w-1/2 relative px-10" key={currClass?.name}>
       <button
         className="w-8 h-8 flex justify-center items-center text-2xl bg-gradient-to-r from-yellow-500 to-red-500 shadow-2xl absolute top-0 right-0 rounded-full"
         onClick={() =>
-          updateSelectedSheet({
-            class: selectedSheet?.class.filter(
-              (c: Class) => c.name !== currClass.name
-            ),
-          })
+          updateSheetWithId(
+            sheetId,
+            {
+              ...sheet?.data,
+              class: sheet.data.class.filter(
+                (c: Class) => c.name !== currClass.name
+              ),
+            },
+            sheet?.campaign,
+            sheet?.user!
+          )
+            .then((res: any) => {
+              console.log(res);
+              setSheet(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            })
         }
       >
         <GiFlamer />
@@ -44,7 +68,7 @@ export const CharacterForm = ({ currClass, classIndex }: CharacterFormArgs) => {
         value={currClass.level}
         onChange={(e) => {
           const newLevel: number = Number(e.target.value);
-          const otherClasses = selectedSheet.class.filter(
+          const otherClasses = sheet.data.class.filter(
             (c: Class, i: number) => i !== classIndex
           );
           const totalOtherLevels = otherClasses.reduce(
@@ -54,25 +78,65 @@ export const CharacterForm = ({ currClass, classIndex }: CharacterFormArgs) => {
 
           if (
             newLevel > 0 &&
-            newLevel <= selectedSheet.level &&
+            newLevel <= sheet?.data?.level &&
             Number(e.target.value) > 0 &&
-            Number(e.target.value) <= selectedSheet.level - totalOtherLevels
+            Number(e.target.value) <= sheet?.data?.level - totalOtherLevels
           ) {
-            const newClass = selectedSheet.class.map((c: Class, i: number) => {
+            const newClass = sheet.data.class.map((c: Class, i: number) => {
               if (currClass.name === c.name) {
                 return { ...c, level: Number(e.target.value) };
               } else {
                 return c;
               }
             });
-            updateSelectedSheet({
-              ...selectedSheet,
-              class: newClass,
-            });
+
+            updateSheetWithId(
+              sheetId,
+              {
+                ...sheet?.data,
+                class: newClass,
+              },
+              sheet?.campaign,
+              sheet?.user!
+            )
+              .then((res: any) => {
+                console.log(res);
+                setSheet(res);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           }
         }}
         placeholder="0"
       />
+      <section>
+        <h4
+          className="text-3xl font-bold"
+          onClick={() => setFeaturesVisible(!featuresVisible)}
+        >
+          Features
+        </h4>
+        <section
+          className={`flex flex-col w-full ${
+            featuresVisible ? "visible" : "invisible"
+          }`}
+        >
+          {currClass.features ? (
+            currClass?.features?.map((feature: any) => {
+              if (feature.level <= currClass.level)
+                return (
+                  <div key={feature.name} className="flex flex-col">
+                    <h5 className="text-xl font-medium">{feature.name}</h5>
+                    <p className="text">{feature.description}</p>
+                  </div>
+                );
+            })
+          ) : (
+            <>WIP</>
+          )}
+        </section>
+      </section>
     </div>
   );
 };
