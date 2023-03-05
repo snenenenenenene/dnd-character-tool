96a2ce9a30e52a1d1db44c42d3e97f38487aa151
snenenenenenene <sennebels@gmail.com>
2023-03-05 09:33:18 +0100

    refactor: :truck: moved logic to modal

--- app/sheets/[id]/page.tsx
+++ app/sheets/[id]/page.tsx
@@ -4,19 +4,24 @@ import React, { useEffect, useState } from "react";
 import _ from "lodash";
 import Link from "next/link";
 import { GiBoomerang, GiBroadheadArrow } from "react-icons/gi";
-import { CharacterForm } from "@/app/components/character/CharacterForm";
+import { ClassEditor } from "@/app/components/character/ClassEditor";
 import { classes } from "@/data/classes/classes";
 import { Class, Expansion } from "@/data/classes/types";
 import { getSheetWithId, updateSheetWithId } from "@/app/utils/apiCalls";
 import Image from "next/image";
 import { toast } from "react-toastify";
+import { Sheet } from "@/app/utils/store";
+import { Modal } from "@/app/components/common/Modal";
+import { Button } from "@/app/components/common/Button";
 
 export default function ClassSelection(context: any) {
   const groups = _.groupBy(classes, "expansion");
-  const [sheet, setSheet] = useState<any>();
+  const [sheet, setSheet] = useState<Sheet>();
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [selectedClass, setSelectedClass] = useState<Class>();
 
   useEffect(() => {
-    getSheetWithId(context.params.id).then((res) => {
+    getSheetWithId(context.params.id).then((res: any) => {
       setSheet(res);
     });
   }, []);
@@ -28,13 +33,15 @@ export default function ClassSelection(context: any) {
           href={`/sheets/${sheet?.id}/race`}
           className="border-b-2 pt-3 border-light-secondary dark:border-dark-secondary pb-5 hover:bg-light-secondary hover:dark:bg-dark-secondary hover:text-light-primary hover:dark:text-dark-primary"
         >
-          <Image
-            src={sheet?.data?.race?.picture}
-            alt={sheet?.data?.race?.name}
-            className="w-40 h-40 object-contain"
-            width={160}
-            height={160}
-          />
+          {sheet?.data?.race.picture && (
+            <Image
+              src={sheet.data.race.picture}
+              alt={sheet.data.race.name}
+              className="w-40 h-40 object-contain"
+              width={160}
+              height={160}
+            />
+          )}
           <h2 className="mx-auto text-xl font-semibold ">
             {sheet?.data?.race?.name}
           </h2>
@@ -52,34 +59,8 @@ export default function ClassSelection(context: any) {
                     key={_class.name}
                     value={_class.name}
                     onClick={() => {
-                      //accumulate all levels of all classes
-                      let total = 0;
-                      sheet?.data?.class?.forEach((c: any) => {
-                        total += c.level;
-                      });
-
-                      if (sheet?.data?.class?.length < 2) {
-                        if (total >= sheet?.data?.level)
-                          return toast.error(
-                            "You have reached the maximum level for your character."
-                          );
-                        updateSheetWithId(
-                          context.params.id,
-                          {
-                            ...sheet?.data,
-                            class: [...sheet.data.class, _class],
-                          },
-                          sheet?.campaign,
-                          sheet?.user!
-                        )
-                          .then((res: any) => {
-                            console.log(res);
-                            setSheet(res);
-                          })
-                          .catch((err) => {
-                            console.log(err);
-                          });
-                      }
+                      setSelectedClass(_class);
+                      setShowModal(true);
                     }}
                   >
                     <h4>{_class.name}</h4>
@@ -92,11 +73,11 @@ export default function ClassSelection(context: any) {
       </section>
 
       <section className="w-full h-full flex px-8 pt-5">
-        <section className="flex w-full h-full gap-5 pr-4">
+        <section className="flex flex-col w-1/2 h-full gap-5 pr-4">
           {sheet?.data?.class &&
             sheet?.data?.class?.length > 0 &&
             sheet?.data?.class?.map((c: Class, i: number) => (
-              <CharacterForm
+              <ClassEditor
                 setSheet={setSheet}
                 sheetId={context.params.id}
                 sheet={sheet}
@@ -105,6 +86,14 @@ export default function ClassSelection(context: any) {
                 key={c.name}
               />
             ))}
+        </section>
+        <section className="flex flex-col w-1/2 h-full">
+          {sheet?.data?.class && sheet?.data?.class?.length > 0 && (
+            <>
+              <p>Max HP</p>
+              <p>{sheet?.data.hitPoints.max}</p>
+            </>
+          )}
         </section>
       </section>
 
@@ -121,6 +110,58 @@ export default function ClassSelection(context: any) {
       >
         <GiBoomerang />
       </Link>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <section className="w-full h-full flex flex-col items-center">
+          <h2 className="text-2xl font-bold">{selectedClass?.name}</h2>
+          {selectedClass?.image && (
+            <Image
+              src={selectedClass?.image}
+              alt={selectedClass?.name}
+              className="w-40 h-40 object-contain"
+              width={160}
+              height={160}
+            />
+          )}
+          <p>{selectedClass?.hitDie}</p>
+          <p>{selectedClass?.proficiencies?.armor}</p>
+          <Button
+            className="w-11/12 mb-5 mt-auto"
+            onClick={() => {
+              let total = 0;
+              sheet?.data?.class?.forEach((c: any) => {
+                total += c.level;
+              });
+
+              if (sheet?.data?.class?.length! < 2) {
+                if (total >= sheet?.data?.level!)
+                  return toast.error(
+                    "You have reached the maximum level for your character."
+                  );
+                updateSheetWithId(
+                  context.params.id,
+                  {
+                    ...sheet?.data,
+                    class: [...sheet?.data?.class!, selectedClass],
+                  },
+                  sheet?.campaign,
+                  sheet?.user!
+                )
+                  .then((res: any) => {
+                    console.log(res);
+                    setSheet(res);
+                    setShowModal(false);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    toast.error("Something went wrong.");
+                  });
+              }
+            }}
+          >
+            Pick Character
+          </Button>
+        </section>
+      </Modal>
     </div>
   );
 }
