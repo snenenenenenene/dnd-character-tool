cd9f447dee030719125a517cae23cd94099d547c
senne <sennebels@gmail.com>
2023-07-28 11:52:51 +0200

    feat: :truck: route redirecting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/sheets",
+        permanent: false,
+      },
+      {
+        source: "/sheets/:id",
+        destination: "/sheets/:id/class",
+        permanent: false,
+      },
+    ];
+  },
   experimental: {
     appDir: true,
   },
   images: {
-    domains: ["www.dndbeyond.com", "rpgrunkleplaysgames.files.wordpress.com"],
+    domains: [
+      "www.dndbeyond.com",
+      "rpgrunkleplaysgames.files.wordpress.com",
+      "source.boringavatars.com",
+    ],
   },
 };
 
