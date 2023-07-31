1fb4fbd7a184563ada61778f0cb1e80df75f7eeb
senne <sennebels@gmail.com>
2023-07-31 11:20:04 +0200

    fix: :ambulance:

--- app/sheets/[id]/gear/page.tsx
+++ app/sheets/[id]/gear/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 export default function Gear() {
   return <div>Gear</div>;
 }
--- app/sheets/[id]/personality/page.tsx
+++ app/sheets/[id]/personality/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function Personality() {
   return <div>Personality</div>;
 }
